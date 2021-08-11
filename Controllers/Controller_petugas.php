a23d43be081dfb6d2dd70fc5b346ec9f60c251db
x01000101x <leodamanik619@gmail.com>
2021-08-11 21:04:33 +0700

    Made the MVC

--- Controllers/Controller_petugas.php
+++ Controllers/Controller_petugas.php
@@ -54,7 +54,7 @@ class Controller_petugas
 
 
     // Method untuk mengambil data seleksi dari table
-    function GetData_Where($id_petugas)
+    function GetData_Where($id_petugas, $password)
     {
 
         // perintah GET data
