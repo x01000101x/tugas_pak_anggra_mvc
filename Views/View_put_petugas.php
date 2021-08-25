09d133cdd2981a48063a6ef0169e3363ee7a3657
x01000101x <leodamanik619@gmail.com>
2021-08-25 13:30:55 +0700

    hidden

--- Views/View_put_petugas.php
+++ Views/View_put_petugas.php
@@ -15,7 +15,7 @@ foreach ($GetPetugas as $Get) {
 ?>
 
     <form action="../Config/Routes.php?function=put_petugas" method="POST">
-        <input type="text" name="csrf_token" value="<?php echo CreateCSRF(); ?>" />
+        <input type="hidden" name="csrf_token" value="<?php echo CreateCSRF(); ?>" />
         <table border="1">
             <input type="hidden" name="id_petugas" value="<?php echo $Get['id_petugas']; ?>">
 
