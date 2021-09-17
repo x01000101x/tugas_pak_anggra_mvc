2cfb732acf35f32949ea145378bd84073834acd2
x01000101x <leodamanik619@gmail.com>
2021-09-17 16:44:00 +0700

    pertemuan 8

--- Views/View_post_siswa.php
+++ Views/View_post_siswa.php
@@ -7,32 +7,32 @@ include('../Config/Csrf.php');
     <table border="1">
         <tr>
             <td>NISN</td>
-            <td><input type="number" name="nisn"></td>
+            <td><input type="number" name="nisn" required></td>
         </tr>
         <tr>
             <td>NIS</td>
-            <td><input type="number" name="nis"></td>
+            <td><input type="number" name="nis" required></td>
         </tr>
         <tr>
             <td>Nama</td>
-            <td><input type="text" name="nama"></td>
+            <td><input type="text" name="nama" required></td>
         </tr>
         <tr>
             <td>ID kelas</td>
-            <td><input type="number" name="id_kelas"></td>
+            <td><input type="number" name="id_kelas" required></td>
         </tr>
         <tr>
             <td>Alamat</td>
-            <td><input type="text" name="alamat">
+            <td><input type="text" name="alamat" required>
             </td>
         <tr>
             <td>No Telepon</td>
-            <td>62<input type="number" name="no_telp">
+            <td>62<input type="number" name="no_telp" required>
             </td>
         </tr>
         <tr>
             <td>ID_SPP</td>
-            <td><input type="number" name="id_spp">
+            <td><input type="number" name="id_spp" required>
             </td>
         </tr>
         <tr>
