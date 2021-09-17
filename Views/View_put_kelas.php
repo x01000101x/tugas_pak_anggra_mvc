a88af431b64c1d11197b58056754bcc71a132c15
x01000101x <leodamanik619@gmail.com>
2021-09-17 17:03:40 +0700

    pertemuan 8

--- Views/View_put_kelas.php
+++ Views/View_put_kelas.php
@@ -19,18 +19,18 @@ foreach ($GetKelas as $Get) {
     <form action="../Config/Routes.php?function=put_kelas" method="POST">
         <input type="hidden" name="csrf_token" value="<?php echo CreateCSRF(); ?>" />
         <table border="1">
-            <input type="hidden" name="id_kelas" required value="<?php echo $Get['id_kelas']; ?>">
+            <input type="hidden" name="id_kelas" value="<?php echo $Get['id_kelas']; ?>">
             <tr>
                 <td>id kelas</td>
-                <td><input type="number" name="id_kelas" value="<?php echo $Get['id_kelas']; ?>"></td>
+                <td><input type="number" name="id_kelas" required value="<?php echo $Get['id_kelas']; ?>"></td>
             </tr>
             <tr>
                 <td>nama kelas</td>
-                <td><input type="text" name="nama_kelas" value="<?php echo $Get['nama_kelas']; ?>"></td>
+                <td><input type="text" name="nama_kelas" required value="<?php echo $Get['nama_kelas']; ?>"></td>
             </tr>
             <tr>
                 <td>kompetensi keahlian</td>
-                <td><input type="text" name="kompetensi_keahlian" value="<?php echo $Get['kompetensi_keahlian']; ?>"></td>
+                <td><input type="text" name="kompetensi_keahlian" required value="<?php echo $Get['kompetensi_keahlian']; ?>"></td>
             </tr>
             <td colspan="2" align="right"><input type="submit" name="proses" value="Create"></td>
             </tr>
