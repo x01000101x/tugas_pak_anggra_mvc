f05f9a35de0f1eb5d3aa5e859c4f412dc851b3a3
x01000101x <leodamanik619@gmail.com>
2021-09-17 16:32:39 +0700

    pertemuan 8

--- Views/View_post_kelas.php
+++ Views/View_post_kelas.php
@@ -7,15 +7,15 @@ include('../Config/Csrf.php');
     <table border="1">
         <tr>
             <td>id kelas</td>
-            <td><input type="number" name="id_kelas"></td>
+            <td><input type="number" name="id_kelas" required></td>
         </tr>
         <tr>
             <td>nama kelas</td>
-            <td><input type="text" name="nama_kelas"></td>
+            <td><input type="text" name="nama_kelas" required></td>
         </tr>
         <tr>
             <td>kompetensi keahlian</td>
-            <td><input type="text" name="kompetensi_keahlian"></td>
+            <td><input type="text" name="kompetensi_keahlian" required></td>
         </tr>
         <tr>
             <td colspan="2" align="right"><input type="submit" name="proses" value="Create"></td>
