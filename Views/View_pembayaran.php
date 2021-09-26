520c6529d2a4177f5992e6cae5eb269536c9a8ee
x01000101x <leodamanik619@gmail.com>
2021-09-26 22:30:11 +0700

    pertemuan 8 - final

--- Views/View_pembayaran.php
+++ Views/View_pembayaran.php
@@ -48,7 +48,7 @@
     <h3><a href="View_post_pembayaran.php">Add Data</a></h3>
 
 
-    <table border="1">
+    <table class="table table-bordered" border="1">
         <tr>
             <th>No</th>
             <th>ID Pembayaran</th>
