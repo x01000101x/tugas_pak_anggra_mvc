3ac9f5e6f30e95a1715f269edc53518e170d61a9
x01000101x <leodamanik619@gmail.com>
2021-10-09 23:59:13 +0700

    pertemuan 8 -final

--- Views/View_post_siswa.php
+++ Views/View_post_siswa.php
@@ -11,7 +11,7 @@
 
 <style>
     body {
-        background-color: white;
+        background-color: black;
     }
 
     p,
@@ -25,6 +25,12 @@
     a {
         color: black;
     }
+
+    .con {
+        background-color: white;
+        padding: 20px;
+        justify-content: center;
+    }
 </style>
 
 <body>
@@ -32,60 +38,46 @@
     // Memanggil fungsi dari CSRF
     include('../Config/Csrf.php');
     ?>
-    <div class="con mt-3 ml-3">
+    <div class="con mt-3 ml-3 position-absolute top-0 start-0">
 
         <form action="../Config/Routes.php?function=create" method="POST">
             <input type="hidden" name="csrf_token" value="<?php echo CreateCSRF(); ?>" />
 
-            <div class="row g-3 align-items-center">
-                <div class="col-auto">
-                    <label for="inputPassword6" class="col-form-label">Password</label>
-                </div>
-                <div class="col-auto">
-                    <input type="password" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline">
-                </div>
-                <div class="col-auto">
-                    <span id="passwordHelpInline" class="form-text">
-                        Must be 8-20 characters long.
-                    </span>
-                </div>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-            <!-- <table border="1">
-            <tr>
-                <td>NISN</td>
-                <td><input type="number" name="nisn" required></td>
-            </tr>
-            <tr>
-                <td>NIS</td>
-                <td><input type="number" name="nis" required></td>
-            </tr>
-            <tr>
-                <td>Nama</td>
-                <td><input type="text" name="nama" required></td>
-            </tr>
-            <tr>
-                <td>ID kelas</td>
-                <td><input type="number" name="id_kelas" required></td>
-            </tr>
-            <tr>
-                <td>Alamat</td>
-            <td><input type="text" name="alamat" required>
-        </td>
-        <tr>
-            <td>No Telepon</td>
-            <td>62<input type="number" name="no_telp" required>
-        </td>
-    </tr>
-    <tr>
-        <td>ID_SPP</td>
-        <td><input type="number" name="id_spp" required>
-    </td>
-</tr>
-<tr>
-    <td colspan="2" align="right"><input type="submit" name="proses" value="Create"></td>
-</tr>
-</table> -->
+            <table border="0">
+                <tr>
+                    <td>NISN</td>
+                    <td><input type="number" name="nisn" required></td>
+                </tr>
+                <tr>
+                    <td>NIS</td>
+                    <td><input type="number" name="nis" required></td>
+                </tr>
+                <tr>
+                    <td>Nama</td>
+                    <td><input type="text" name="nama" required></td>
+                </tr>
+                <tr>
+                    <td>ID kelas</td>
+                    <td><input type="number" name="id_kelas" required></td>
+                </tr>
+                <tr>
+                    <td>Alamat</td>
+                    <td><input type="text" name="alamat" required>
+                    </td>
+                <tr>
+                    <td>No Telepon (62) </td>
+                    <td><input type="number" name="no_telp" required>
+                    </td>
+                </tr>
+                <tr>
+                    <td>ID_SPP</td>
+                    <td><input type="number" name="id_spp" required>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2" align="right"><input type="submit" name="proses" value="Create"></td>
+                </tr>
+            </table>
         </form>
     </div>
 
