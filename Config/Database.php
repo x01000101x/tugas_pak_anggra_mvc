3395b39b6fcfb1f183b185712df8f9965a119a51
x01000101x <leodamanik619@gmail.com>
2021-10-16 23:19:26 +0700

    pertemuan 8 -final

--- Config/Database.php
+++ Config/Database.php
@@ -4,11 +4,18 @@
 class Database
 {
 
-	// Property
-	var $host = "db4free.net";
-	var $uname = "siswasiswirk4";
-	var $pass = "akubisacoding";
-	var $db = "pbo_pak_anggra_4";
+	// Online DB
+	// var $host = "db4free.net";
+	// var $uname = "siswasiswirk4";
+	// var $pass = "akubisacoding";
+	// var $db = "pbo_pak_anggra_4";
+	// var $connection;
+
+	//Local DB
+	var $host = "localhost";
+	var $uname = "root";
+	var $pass = "";
+	var $db = "learn_smkn4";
 	var $connection;
 
 	// Method koneksi kedalam database
